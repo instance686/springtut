5c6e9a288e15be53f52063232231b7899cecf3b9
Ayush Srivastava <ayush.cir@gmail.com>
2018-06-04 01:34:07 +0530

    PreConstruct and PostDestroy

--- src/main/java/com/in28min/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28min/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.in28min.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -9,6 +14,10 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+	
+
+	private Logger LOGGER=LoggerFactory
+			.getLogger(BinarySearchImpl.class); 
 
 	@Autowired
 	@Qualifier("bubble-sort")
@@ -18,5 +27,18 @@ public class BinarySearchImpl {
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);
 		return 3;
+	}
+	
+	@PostConstruct//called after dependencies are populated
+	public void postConstruct() {
+		LOGGER.info("postConstruct");
+		
+	}
+	
+
+	@PreDestroy//called before destroying of beans
+	public void preDestryoy() {
+		LOGGER.info("preDestryoy");
+		
 	}
 }
