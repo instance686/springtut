bcb6b8827942e9e8e1c904ba65c8b39a64476bc1
Ayush Srivastava <ayush.cir@gmail.com>
2018-06-04 02:11:33 +0530

    Spring without spring boot

--- src/main/java/com/in28min/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/in28min/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -1,17 +1,20 @@
 package com.in28min.spring.basics.springin5steps;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28min.spring.basics.springin5steps.basic.BinarySearchImpl;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.in28min.spring.basics.springin5steps")
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
 	
-		ApplicationContext context=SpringApplication.run(SpringIn5StepsApplication.class, args);
+		ApplicationContext context=new AnnotationConfigApplicationContext(SpringIn5StepsApplication.class);
 		BinarySearchImpl binarySearch=context.getBean(BinarySearchImpl.class);
 		System.out.println(binarySearch);
 		BinarySearchImpl binarySearch1=context.getBean(BinarySearchImpl.class);
